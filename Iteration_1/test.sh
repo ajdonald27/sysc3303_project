01e3603ebc222f15d1dd6a9d2ee67fb55e165189
ajdonald27 <ajdonald@cmail.carleton.ca>
2025-01-30 21:56:29 -0500

    added README.md, updated test.sh

--- Iteration_1/test.sh
+++ Iteration_1/test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo Running trace.txt test
-g++ monitor_implementation.cpp -o elevator
+g++ -std=c++11 monitor_implementation.cpp -o elevator
 ./elevator
 
 echo Running Unit Tests
 
-g++ -DUNIT_TESTING unit_tests.cpp monitor_implementation.cpp -o unit_tests -pthread
+g++ -std=c++11 -DUNIT_TESTING unit_tests.cpp monitor_implementation.cpp -o unit_tests -pthread
 ./unit_tests
